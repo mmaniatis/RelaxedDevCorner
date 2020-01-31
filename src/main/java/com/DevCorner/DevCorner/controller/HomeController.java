337d0f11d52fbd6dc0e1b739e899225d2b16dbd4
Michael Maniatis <Mikejohnmaniatis@gmail.com>
2020-01-31 16:48:10 -0600

    Starting to design .. Added bootstrap/thymeleaf as my front end..

--- src/main/java/com/DevCorner/DevCorner/controller/HomeController.java
+++ src/main/java/com/DevCorner/DevCorner/controller/HomeController.java
@@ -1,13 +1,15 @@
-package com.DevCorner.DevCorner.Controllers;
+package com.DevCorner.DevCorner.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
 public class HomeController {
 
     @RequestMapping("/")
     public String index() {
-        return "Hello World!!!!!!!";
+        return "home/index";
     }
 }
