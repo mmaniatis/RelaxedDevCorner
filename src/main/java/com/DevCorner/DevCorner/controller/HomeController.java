7a2d4d608d18f9e9a71599559bcb7bc452a42605
Michael Maniatis <Mikejohnmaniatis@gmail.com>
2020-02-02 22:42:21 -0600

    Preparing solution for Azure CosmosDB.

--- src/main/java/com/DevCorner/DevCorner/controller/HomeController.java
+++ src/main/java/com/DevCorner/DevCorner/controller/HomeController.java
@@ -1,9 +1,9 @@
 package com.DevCorner.DevCorner.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+        import org.springframework.stereotype.Controller;
+        import org.springframework.web.bind.annotation.ResponseBody;
+        import org.springframework.web.bind.annotation.RestController;
+        import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
