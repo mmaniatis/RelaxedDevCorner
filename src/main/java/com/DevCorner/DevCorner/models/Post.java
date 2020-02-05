4efaceb4364efb09e3cfc60cf2d246b8cd0ace03
Michael Maniatis <Mikejohnmaniatis@gmail.com>
2020-02-04 22:08:10 -0600

    Adding MongoDB to the blog.

---
+++ src/main/java/com/DevCorner/DevCorner/models/Post.java
@@ -0,0 +1,15 @@
+package com.DevCorner.DevCorner.models;
+
+import java.util.Date;
+
+public class Post {
+
+    public int id;
+    public String title;
+    public String slug;
+    public String body;
+    public String author;
+    public Date cdDate;
+
+    public Post(){ }
+}
