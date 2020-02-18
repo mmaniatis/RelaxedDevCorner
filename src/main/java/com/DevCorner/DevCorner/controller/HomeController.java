7e975856253007609670554f67cc30a8860f326b
Michael Maniatis <Mikejohnmaniatis@gmail.com>
2020-02-17 22:57:26 -0600

    Attempting to pass the model to view using Thymeleaf.

--- src/main/java/com/DevCorner/DevCorner/controller/HomeController.java
+++ src/main/java/com/DevCorner/DevCorner/controller/HomeController.java
@@ -5,11 +5,8 @@ package com.DevCorner.DevCorner.controller;
         import org.springframework.beans.factory.annotation.Autowired;
         import org.springframework.boot.autoconfigure.SpringBootApplication;
         import org.springframework.stereotype.Controller;
-        import org.springframework.web.bind.annotation.ResponseBody;
-        import org.springframework.web.bind.annotation.RestController;
         import org.springframework.web.bind.annotation.RequestMapping;
-
-        import java.util.Date;
+        import org.springframework.web.servlet.ModelAndView;
         import java.util.List;
 
 @Controller
@@ -19,18 +16,14 @@ public class HomeController {
     private PostRepository repository;
 
     @RequestMapping("/")
-    public String index() {
-        List<Post> postList = GetPosts(".NET");
-        return "home/index";
+    public ModelAndView index() {
+        ModelAndView mav = new ModelAndView("home/index")
+                .addObject("postList", GetPosts(".NET"));
+        return mav;
     }
 
     public List<Post> GetPosts(String Category)
     {
-        if (repository.findPostsByCategory(".NET").size() == 0)
-        {
-            repository.save(new Post(".NET", "Working in Java after a career of .NET",
-                    "Transition", "Example Body", "Michael J. Maniatis", new Date()));
-        }
-        return repository.findPostsByCategory((".NET"));
+        return repository.findPostsByCategory(Category);
     }
 }
