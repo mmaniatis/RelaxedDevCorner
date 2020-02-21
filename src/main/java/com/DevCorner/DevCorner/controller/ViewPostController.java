54db2fd2c699b3e92a55673ca78dbbfc8545c187
Michael Maniatis <Mikejohnmaniatis@gmail.com>
2020-02-20 22:45:09 -0600

    ViewPost controller hitting from Thymeleaf url.

--- src/main/java/com/DevCorner/DevCorner/controller/ViewPostController.java
+++ src/main/java/com/DevCorner/DevCorner/controller/ViewPostController.java
@@ -8,9 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Controller
 @SpringBootApplication
 public class ViewPostController {
