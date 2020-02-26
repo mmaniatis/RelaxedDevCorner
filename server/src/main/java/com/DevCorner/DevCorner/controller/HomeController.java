e8e7f48540bc36fe02d87d354bebcf0bf3270087
Mike Maniatis <mikejohnmaniatis@gmail.com>
2020-02-25 19:41:19 -0600

    Pivoting to a React front end/spring boot api

--- server/src/main/java/com/DevCorner/DevCorner/controller/HomeController.java
+++ server/src/main/java/com/DevCorner/DevCorner/controller/HomeController.java
@@ -5,27 +5,22 @@ package com.DevCorner.DevCorner.controller;
         import org.springframework.beans.factory.annotation.Autowired;
         import org.springframework.boot.autoconfigure.SpringBootApplication;
         import org.springframework.stereotype.Controller;
-        import org.springframework.web.bind.annotation.RequestMapping;
+        import org.springframework.web.bind.annotation.*;
         import org.springframework.web.servlet.ModelAndView;
 
+        import javax.validation.Valid;
         import java.util.ArrayList;
         import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping("/api")
 @SpringBootApplication
 public class HomeController {
     @Autowired
     private PostRepository repository;
-
-    @RequestMapping("/")
-    public ModelAndView index() {
-        ModelAndView mav = new ModelAndView("home/index")
-                .addObject("postList", GetPosts("Development"));
-        return mav;
-    }
-
-    public ArrayList<Post> GetPosts(String Category)
+    @GetMapping("/GetPosts/{category}")
+    public ArrayList<Post> GetPosts(@PathVariable String category)
     {
-        return repository.findPostsByCategory(Category);
+        return repository.findPostsByCategory(category);
     }
 }
