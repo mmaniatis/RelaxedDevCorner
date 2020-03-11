109d88a1f412ef5df309f3a0a0406c16f4a6330a
Mike Maniatis <mikejohnmaniatis@gmail.com>
2020-03-10 21:22:15 -0500

    Finishing up the UI for social sign in

--- client/app/src/App.js
+++ client/app/src/App.js
@@ -5,6 +5,25 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import CreateAccount from './components/CreateAccount';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = { isAuthenticated: false, user: null, token: ''};
+  }
+
+  onSuccess = (response) => {
+    const token = response.headers.get('x-auth-token');
+    response.json().then(user => {
+      if (token) {
+        this.setState({isAuthenticated: true, user: user, token: token});
+      }
+    });
+  };
+  
+  onFailed = (error) => {
+    alert(error);
+  };
+
+  
   render() {
     return (
       <Router>
--- client/app/src/components/AppNavBar.js
+++ client/app/src/components/AppNavBar.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import { Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink, ButtonGroup, Button} from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 export default class AppNavbar extends Component {
   constructor(props) {
     super(props);
-    this.state = {isOpen: false};
+    this.state = {isOpen: false, isSignedIn: false};
     this.toggle = this.toggle.bind(this);
   }
 
@@ -15,20 +15,37 @@ export default class AppNavbar extends Component {
     });
   }
 
+  GetSignInButton() {
+    if (this.state.isSignedIn === false)
+    {
+      return <ButtonGroup>
+                <Button color="primary" size="sm">Sign in with Google</Button>
+            </ButtonGroup>
+    }
+    else
+    {
+      return  <ButtonGroup>
+                <Button color="primary" size="sm" disabled="true">Your signed in !</Button>
+              </ButtonGroup>
+    }
+  }
   render() {
+
     return <Navbar color="dark" dark expand="md">
       <NavbarBrand tag={Link} to="/">Home</NavbarBrand>
       <NavbarToggler onClick={this.toggle}/>
       <Collapse isOpen={this.state.isOpen} navbar>
         <Nav className="ml-auto" navbar>
-          <NavItem>
-            <NavLink href="/CreateAccount">Create an Account</NavLink>
-          </NavItem>
+
           <NavItem>
             <NavLink href="https://twitter.com/michaelmaniatis">@mikedev</NavLink>
           </NavItem>
           <NavItem>
             <NavLink href="https://github.com/mikejohnmaniatis">GitHub</NavLink>
+          </NavItem>
+
+          <NavItem>
+            {this.GetSignInButton()}
           </NavItem>
         </Nav>
       </Collapse>
