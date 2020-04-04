6f68cb715783f135c548f2d71228c5ee7acc9675
Michael Maniatis <Mikejohnmaniatis@gmail.com>
2020-04-04 18:28:07 -0500

    Following google api documentation to authenticate the user ..

--- server/src/main/java/com/DevCorner/DevCorner/controller/AccountController.java
+++ server/src/main/java/com/DevCorner/DevCorner/controller/AccountController.java
@@ -1,16 +1,19 @@
 package com.DevCorner.DevCorner.controller;
 
 import com.DevCorner.DevCorner.models.*;
-import com.DevCorner.DevCorner.repository.IPostRepository;
-import com.DevCorner.DevCorner.repository.PostRepository;
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.interfaces.Claim;
-import com.auth0.jwt.interfaces.DecodedJWT;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import jdk.internal.net.http.websocket.Transport;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
 
 @RestController
 @SpringBootApplication
@@ -20,18 +23,48 @@ public class AccountController {
     /*
     * eyJhbGciOiJSUzI1NiIsImtpZCI6IjI1N2Y2YTU4MjhkMWU0YTNhNmEwM2ZjZDFhMjQ2MWRiOTU5M2U2MjQiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJhY2NvdW50cy5nb29nbGUuY29tIiwiYXpwIjoiMTcwMDE3NTg2Njc2LTJwMWUyY3BmMGpndDhiMTk0NmNybjIwaXBkdWxpNGc1LmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwiYXVkIjoiMTcwMDE3NTg2Njc2LTJwMWUyY3BmMGpndDhiMTk0NmNybjIwaXBkdWxpNGc1LmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTExMzkxMTU1MzkzNTUzMTkzMzAzIiwiZW1haWwiOiJtaWtlam9obm1hbmlhdGlzQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhdF9oYXNoIjoidGx0ZEZqZnpFZS1zSURuU3hUM0RRQSIsIm5hbWUiOiJNaWtlIE1hbmlhdGlzIiwicGljdHVyZSI6Imh0dHBzOi8vbGg2Lmdvb2dsZXVzZXJjb250ZW50LmNvbS8td2k3SEJkc3lzVE0vQUFBQUFBQUFBQUkvQUFBQUFBQUFBQUEvQUFLV0pKTU11YjV5aE84X1RuQ25MQWdSN09GSm9RVWpXUS9zOTYtYy9waG90by5qcGciLCJnaXZlbl9uYW1lIjoiTWlrZSIsImZhbWlseV9uYW1lIjoiTWFuaWF0aXMiLCJsb2NhbGUiOiJlbiIsImlhdCI6MTU4NjAxOTI5MCwiZXhwIjoxNTg2MDIyODkwLCJqdGkiOiIxNTVkMTM4MmZkODFlZjVlMjY5ZTg3ODE5ZTU2YTBkNjcwN2ZlZmJlIn0.YJaUUfkvL3o84TCgAy7JuYtLxPusEm5eRwmRkxyOV5AfJeX13QKRqTqx6irQmljJcx6ULA42o9uVIFASbSXaucsvnlwf1z_ahIWaq1rOfYiVkehrSpgsX2EPEKputsATCN8aFfJTvpsiKACVaDD1fK7T_3MVH6Mx2NQ73KikYnxcXn4N2z2RlrLJOUsmcy3WJanpgx5PyLLWAkxmHo0D9CZftP8UwQoDRhLiFFIAiScxaaJTwGQsWw1ii4jGWGokEjDlNBih5ru4OmD2vxI1A6EF7-65-sZah0nuSsuh--y_YgILdxb9dZShzBmH82XNiZRkGQ73eIQiF-EwdvvuxA
     * */
-
     @GetMapping("/Authenticate")
-    public String Authenticate(String token)
+    public boolean Authenticate(String idTokenString)
     {
-        DecodedJWT jwt = JWT.decode(token);
+        HttpTransport transport = new NetHttpTransport();
+        JacksonFactory jsonFactory = new JacksonFactory();
+        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(transport, jsonFactory)
+                // Specify the CLIENT_ID of the app that accesses the backend:
+                .setAudience(Collections.singletonList("170017586676-2p1e2cpf0jgt8b1946crn20ipduli4g5.apps.googleusercontent.com"))
+                // Or, if multiple clients access the backend:
+                //.setAudience(Arrays.asList(CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3))
+                .build();
 
-        String issuer = jwt.getIssuer();
-        String email = jwt.getClaim("email").asString();
-        String name = jwt.getClaim("name").asString();
-        String locale = jwt.getClaim("locale").asString();
-        //Boolean isEmailVerified = jwt.getClaim("email_verified")x.asBoolean();
-        return "true";
+// (Receive idTokenString by HTTPS POST)
+        try {
+            GoogleIdToken idToken = verifier.verify(idTokenString);
+            if (idToken != null) {
+                Payload payload = idToken.getPayload();
+
+                // Print user identifier
+                String userId = payload.getSubject();
+                System.out.println("User ID: " + userId);
+
+                // Get profile information from payload
+                String email = payload.getEmail();
+                boolean emailVerified = Boolean.valueOf(payload.getEmailVerified());
+                String name = (String) payload.get("name");
+                String pictureUrl = (String) payload.get("picture");
+                String locale = (String) payload.get("locale");
+                String familyName = (String) payload.get("family_name");
+                String givenName = (String) payload.get("given_name");
+
+                // Use or store profile information
+                // ...
+
+            } else {
+                return false;
+            }
+        }
+        catch (GeneralSecurityException | IOException Ex){
+            return  false;
+        }
+        return true;
     }
 
 //    private boolean CreateUserIfNotExists(Account account)
