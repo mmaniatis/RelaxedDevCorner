b581382cf7914e4075afb2cade45979f33b12d59
Michael Maniatis <Mikejohnmaniatis@gmail.com>
2020-04-13 20:17:09 -0500

    prod release

---
+++ server/src/main/resources/static/precache-manifest.6c35151f4c440eda36bfa3b1fd99d613.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2c48cbd6df61b3ae3588c37962f9415",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7458db4e81fd0b82622b",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "dda0cde0bc86e815d571",
+    "url": "/static/css/main.2ff46b65.chunk.css"
+  },
+  {
+    "revision": "7458db4e81fd0b82622b",
+    "url": "/static/js/2.b3754ecf.chunk.js"
+  },
+  {
+    "revision": "ed8b6bdf10a485188d1c90d4d71fc715",
+    "url": "/static/js/2.b3754ecf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dda0cde0bc86e815d571",
+    "url": "/static/js/main.48af031e.chunk.js"
+  },
+  {
+    "revision": "a0478af65bd914356dbc",
+    "url": "/static/js/runtime-main.4032331f.js"
+  }
+]);
