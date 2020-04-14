1d0f336d6f199b4db2d521812af95fc410fcc604
Mike Maniatis <mikejohnmaniatis@gmail.com>
2020-04-14 13:19:48 -0500

    prod release'

---
+++ server/src/main/resources/static/precache-manifest.dece3f84a2ead4b94aab011bba67a7ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "062095bd313c8fd6f0de9c4fdb321971",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f870c5e40189fed2a717",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "b6a3b15d8e8c5c5af67b",
+    "url": "/static/css/main.cdf97753.chunk.css"
+  },
+  {
+    "revision": "f870c5e40189fed2a717",
+    "url": "/static/js/2.f9df6ffa.chunk.js"
+  },
+  {
+    "revision": "ed8b6bdf10a485188d1c90d4d71fc715",
+    "url": "/static/js/2.f9df6ffa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6a3b15d8e8c5c5af67b",
+    "url": "/static/js/main.619c07a5.chunk.js"
+  },
+  {
+    "revision": "a0478af65bd914356dbc",
+    "url": "/static/js/runtime-main.4032331f.js"
+  }
+]);
--- server/src/main/resources/static/service-worker.js
+++ server/src/main/resources/static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6c35151f4c440eda36bfa3b1fd99d613.js"
+  "/precache-manifest.dece3f84a2ead4b94aab011bba67a7ad.js"
 );
 
 self.addEventListener('message', (event) => {
