1e59d134bbae8d4e873b6dc051c9510560f63d71
Michael Maniatis <mikejohnmaniatis@gmail.com>
2020-04-19 17:38:10 -0500

    finished the two sum algorithm.

--- server/src/main/java/com/DevCorner/DevCorner/repository/AlgorithmRepository.java
+++ server/src/main/java/com/DevCorner/DevCorner/repository/AlgorithmRepository.java
@@ -5,9 +5,7 @@ import com.DevCorner.DevCorner.models.TreeNode;
 
 
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class AlgorithmRepository implements IAlgorithmRepository {
 
@@ -18,18 +16,24 @@ public class AlgorithmRepository implements IAlgorithmRepository {
 
     public ArrayList<Integer> TwoSum(ArrayList<Integer> intArray, Integer target)
     {
+        ArrayList<Integer> sums = new ArrayList<>(); //arrayList we will return
+        HashMap<Integer, Integer> hash = new HashMap<>(); //hash table to track the pairings
         for (int i = 0; i < intArray.size(); i++)
         {
-            for (int j = 0; j < intArray.size(); j++)
+            if (hash.containsKey(intArray.get(i))) // if our hash has this number as a key, it means we found the other pair already
             {
-                if (intArray.get(i) + intArray.get(j) == target)
-                {
-                    return new ArrayList<Integer>(Arrays.asList(i, j));
-                }
+                sums.add(hash.get(intArray.get(i))); //return the pair
+                sums.add(i);
+                return sums;
+            }
+            else //no pair has been found yet
+            {
+                int sumMinusTarget = target - intArray.get(i); //target - current index, add to hash table for future use
+                hash.put(sumMinusTarget, i);
             }
         }
         
-        return new ArrayList<Integer>();
+        return sums; //return the pair if any
     }
 
     /* Helper Methods */
