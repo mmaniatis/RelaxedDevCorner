22a80fb2ec43b78d511abbfb26f83142fd5fa065
Michael Maniatis <mikejohnmaniatis@gmail.com>
2020-04-23 08:59:44 -0500

    Test

--- server/src/main/java/com/DevCorner/DevCorner/Config.java
+++ server/src/main/java/com/DevCorner/DevCorner/Config.java
@@ -23,30 +23,11 @@ public class Config implements WebMvcConfigurer {
         registry.addResourceHandler("/**")
                 .resourceChain(true)
                 .addResolver(resolver);
-
-        // Can try to play with
-        // registry.addResourceHandler("/**")
-        //        .addResourceLocations("classpath:/static/");
-        // But this option can't map every path to index.html
-        // Can try https://stackoverflow.com/a/42998817/1032167
-        // to resolve this, but then you loose /api/** => rest
-        // and to be honest it is some regex madness, so
-        // it was easier for me to setup custom resource resolver
-
-
     }
 
     public class ReactResourceResolver implements ResourceResolver {
-        // this is the same directory you are using in package.json
-        // "build-spring": "react-scripts build &&  mv build static && mv static ../src/main/resources",
-        // example REACT_DIR/index.html
         private static final String REACT_DIR = "/static/";
-
-        // this is directory inside REACT_DIR for react static files
-        // example REACT_DIR/REACT_STATIC_DIR/js/
-        // example REACT_DIR/REACT_STATIC_DIR/css/
         private static final String REACT_STATIC_DIR = "static";
-
         private Resource index = new ClassPathResource(REACT_DIR + "index.html");
         private List<String> rootStaticFiles = Arrays.asList("favicon.io",
                 "asset-manifest.json", "manifest.json", "service-worker.js");
