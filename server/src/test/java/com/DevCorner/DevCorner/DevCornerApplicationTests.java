cb028cea5f241153caf7f503767ba5ea9df10433
Mike Maniatis <mikejohnmaniatis@gmail.com>
2020-05-12 18:03:57 -0500

    Adding some tests

--- server/src/test/java/com/DevCorner/DevCorner/DevCornerApplicationTests.java
+++ server/src/test/java/com/DevCorner/DevCorner/DevCornerApplicationTests.java
@@ -1,13 +1,26 @@
 package com.DevCorner.DevCorner;
 
+import com.DevCorner.DevCorner.models.Post;
+import com.DevCorner.DevCorner.repository.IPostRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class DevCornerApplicationTests {
+	@Autowired
+	private IPostRepository postRepo;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void getPosts() {
+		List<Post> posts = postRepo.GetAllPosts();
+		assert(posts.size() > 0 );
 	}
 
 }
