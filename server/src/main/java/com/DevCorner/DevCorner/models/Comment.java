20d875c5008e303d67d45ac2261bd199b39cdaa5
mmaniatis <michael-maniatis@uiowa.edu>
2020-06-02 15:49:51 -0500

    Adding comment functionality. Basic test/code

---
+++ server/src/main/java/com/DevCorner/DevCorner/models/Comment.java
@@ -0,0 +1,13 @@
+package com.DevCorner.DevCorner.models;
+
+public class Comment {
+    private String body;
+
+    public Comment(String body) {
+        this.body = body;
+    }
+
+    public String getBody(){
+        return body;
+    }
+}
--- server/src/test/java/com/DevCorner/DevCorner/DevCornerApplicationTests.java
+++ server/src/test/java/com/DevCorner/DevCorner/DevCornerApplicationTests.java
@@ -1,14 +1,19 @@
 package com.DevCorner.DevCorner;
 
+import com.DevCorner.DevCorner.models.Comment;
 import com.DevCorner.DevCorner.models.Post;
 import com.DevCorner.DevCorner.repository.IPostRepository;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class DevCornerApplicationTests {
@@ -45,7 +50,17 @@ class DevCornerApplicationTests {
 
 	@Test
 	void getPostByCategory(){
-		ArrayList<Post> posts = postRepo.getPostsByCategory("Development");
+		ArrayList<Post> posts = postRepo.getPostsByCategory("Java");
 		assert(posts.size() > 0);
+	}
+
+	@Test
+	void getComments() {
+		Post p = new Post("TestCategory", "TestTitle",
+				"TestSlug", "TestBody", "TestAuthor", new Date());
+		p.addComment(new Comment("TestBody"));
+		List<Comment> comments = p.getComments();
+
+		assertEquals("TestBody", comments.get(0).getBody());
 	}
 }
